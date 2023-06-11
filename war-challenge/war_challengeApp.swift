520e85ad588ca7f61e03cdda676994b344d16bc3
clf94-dev <clf94@hotmail.es>
2023-06-11 18:38:18 +0200

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Carmen Lucas on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
