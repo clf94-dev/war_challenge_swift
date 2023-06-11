28f8ca9f8023abbf5f6d8a771c93a8afb6db3637
clf94-dev <clf94@hotmail.es>
2023-06-11 18:58:25 +0200

    Setup base ui view

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card12")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(10)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(10)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }.foregroundColor(Color.white)
+                    .font(.system(size: 20))
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
